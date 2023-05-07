3e683ce1ac4653eaea369bd988fa99a391d5de1f
9Batya <81380741+9Batya@users.noreply.github.com>
2023-05-07 21:49:41 +0500

    Update task_5_1b.py

--- exercises/05_basic_scripts/task_5_1b.py
+++ exercises/05_basic_scripts/task_5_1b.py
@@ -25,6 +25,7 @@ $ python task_5_1b.py
 решить без использования условия if.
 """
 
+#!/usr/bin/env python3
 london_co = {
     "r1": {
         "location": "21 New Globe Walk",
@@ -50,3 +51,9 @@ london_co = {
         "routing": True,
     },
 }
+device=str(input('enter device name: '))
+item=str(input('enter item name '+', '.join(london_co[device].keys())+': '))
+print ('\n'+'-'*30)
+print(london_co[device])
+print ('\n'+'-'*30)
+print(london_co[device][item])
