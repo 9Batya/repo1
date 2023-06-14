419f6709c3470fe75c17cd1cc1249d5e2daa6ddb
9Batya <81380741+9Batya@users.noreply.github.com>
2023-06-15 02:19:30 +0500

    Update task_6_2b.py

--- exercises/06_control_structures/task_6_2b.py
+++ exercises/06_control_structures/task_6_2b.py
@@ -12,3 +12,26 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+ip = input('Введите ип: ')
+ip = ip.split('.')
+while True:
+    if any( i.isdigit() == False for i in ip ):
+        print( 'Неправильный IP-адрес' )
+    elif len(ip)!=4:
+        print('Неправильный IP-адрес')
+    elif any(int(i)>255 or int(i)<0 for i in ip):
+        print ('Неправильный IP-адрес')
+    else:
+        break
+    ip = input('Введите ип: ')
+    ip = ip.split( '.' )
+if int(ip[0]) <= 224 and int(ip[1]) != 0:
+    print('unicast')
+elif int(ip[0]) <= 239 and int(ip[1]) != 0:
+    print('multicast')
+elif all(int(i) == 255 for i in ip):
+    print('local broadcast')
+elif all(int(i) == 0 for i in ip):
+    print('unassigned')
+else:
+    print('unused')
