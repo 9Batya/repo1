3ccfeb73b2b3b8c50afddbbbd6209c1f1fd2b839
9Batya <daniel_gariev@mail.ru>
2024-02-04 17:22:19 +0500

    bl

--- exercises/22_oop_basics/task_22_1b.py
+++ exercises/22_oop_basics/task_22_1b.py
@@ -1,65 +1,41 @@
-# -*- coding: utf-8 -*-
+from pprint import pprint
+import logging
+logging.basicConfig(
+    format = '%(threadName)s %(name)s %(levelname)s: %(message)s',
+    level=logging.INFO)
+class Topology:
+    def __init__(self, topology_dict):
+        self.topology = self._normalize(topology_dict)
+    def _normalize(self, topology_dict):
+        return {
+            min(local, remote): max(local, remote)
+            for local, remote in topology_dict.items()
+        }
+    def delete_link(self, *args):
+        links = list(args)
+        for link in links:
+            if link in self.topology.keys():
+                if self.topology.get(self.topology.get(link)) == link:
+                    self.topology.pop(self.topology.get(link))
+                self.topology.pop(link)
+            else:
+                print('Такого соединения нет')
 
-"""
-Задание 22.1b
 
-Изменить класс Topology из задания 22.1a или 22.1.
 
-Добавить метод delete_link, который удаляет указанное соединение.
-Метод должен удалять и "обратное" соединение, если оно есть (ниже пример).
 
-Если такого соединения нет, выводится сообщение "Такого соединения нет".
+topology_example = {('R1', 'Eth0/0'): ('SW1', 'Eth0/1'),
+                    ('R2', 'Eth0/0'): ('SW1', 'Eth0/2'),
+                    ('R2', 'Eth0/1'): ('SW2', 'Eth0/11'),
+                    ('R3', 'Eth0/0'): ('SW1', 'Eth0/3'),
+                    ('R3', 'Eth0/1'): ('R4', 'Eth0/0'),
+                    ('R3', 'Eth0/2'): ('R5', 'Eth0/0'),
+                    ('SW1', 'Eth0/1'): ('R1', 'Eth0/0'),
+                    ('SW1', 'Eth0/2'): ('R2', 'Eth0/0'),
+                    ('SW1', 'Eth0/3'): ('R3', 'Eth0/0')}
 
-Создание топологии
-In [7]: t = Topology(topology_example)
 
-In [8]: t.topology
-Out[8]:
-{('R1', 'Eth0/0'): ('SW1', 'Eth0/1'),
- ('R2', 'Eth0/0'): ('SW1', 'Eth0/2'),
- ('R2', 'Eth0/1'): ('SW2', 'Eth0/11'),
- ('R3', 'Eth0/0'): ('SW1', 'Eth0/3'),
- ('R3', 'Eth0/1'): ('R4', 'Eth0/0'),
- ('R3', 'Eth0/2'): ('R5', 'Eth0/0')}
-
-Удаление линка:
-In [9]: t.delete_link(('R3', 'Eth0/1'), ('R4', 'Eth0/0'))
-
-In [10]: t.topology
-Out[10]:
-{('R1', 'Eth0/0'): ('SW1', 'Eth0/1'),
- ('R2', 'Eth0/0'): ('SW1', 'Eth0/2'),
- ('R2', 'Eth0/1'): ('SW2', 'Eth0/11'),
- ('R3', 'Eth0/0'): ('SW1', 'Eth0/3'),
- ('R3', 'Eth0/2'): ('R5', 'Eth0/0')}
-
-Удаление "обратного" линка:
-в словаре есть запись ``('R3', 'Eth0/2'): ('R5', 'Eth0/0')``, но вызов delete_link
-с указанием ключа и значения в обратном порядке, должно удалять соединение:
-
-In [11]: t.delete_link(('R5', 'Eth0/0'), ('R3', 'Eth0/2'))
-
-In [12]: t.topology
-Out[12]:
-{('R1', 'Eth0/0'): ('SW1', 'Eth0/1'),
- ('R2', 'Eth0/0'): ('SW1', 'Eth0/2'),
- ('R2', 'Eth0/1'): ('SW2', 'Eth0/11'),
- ('R3', 'Eth0/0'): ('SW1', 'Eth0/3')}
-
-Если такого соединения нет, выводится сообщение:
-In [13]: t.delete_link(('R5', 'Eth0/0'), ('R3', 'Eth0/2'))
-Такого соединения нет
-
-"""
-
-topology_example = {
-    ("R1", "Eth0/0"): ("SW1", "Eth0/1"),
-    ("R2", "Eth0/0"): ("SW1", "Eth0/2"),
-    ("R2", "Eth0/1"): ("SW2", "Eth0/11"),
-    ("R3", "Eth0/0"): ("SW1", "Eth0/3"),
-    ("R3", "Eth0/1"): ("R4", "Eth0/0"),
-    ("R3", "Eth0/2"): ("R5", "Eth0/0"),
-    ("SW1", "Eth0/1"): ("R1", "Eth0/0"),
-    ("SW1", "Eth0/2"): ("R2", "Eth0/0"),
-    ("SW1", "Eth0/3"): ("R3", "Eth0/0"),
-}
+if __name__ == "__main__":
+    T = Topology(topology_example)
+    T.delete_link(('R1', 'Eth0/0'), ('R1', 'Eth0/0'))
+    pprint(T.topology)
