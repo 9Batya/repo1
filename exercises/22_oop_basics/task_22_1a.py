435713bb97dbc64b37eb3b8de3d0db47c4a161d4
9Batya <daniel_gariev@mail.ru>
2024-02-04 13:09:39 +0500

    normalin

--- exercises/22_oop_basics/task_22_1a.py
+++ exercises/22_oop_basics/task_22_1a.py
@@ -1,28 +1,25 @@
-# -*- coding: utf-8 -*-
-
-"""
-Задание 22.1a
-
-Скопировать класс Topology из задания 22.1 и изменить его.
-
-Перенести функциональность удаления "дублей" в метод _normalize.
-При этом метод __init__ должен выглядеть таким образом:
-"""
-
-
 class Topology:
     def __init__(self, topology_dict):
         self.topology = self._normalize(topology_dict)
+    def _normalize(self, topology):
+        normalize = {}
+        for local, remote in topology.items():
+            if not normalize.get(remote) == local:
+                normalize[local] = remote
+        return normalize
 
 
-topology_example = {
-    ("R1", "Eth0/0"): ("SW1", "Eth0/1"),
-    ("R2", "Eth0/0"): ("SW1", "Eth0/2"),
-    ("R2", "Eth0/1"): ("SW2", "Eth0/11"),
-    ("R3", "Eth0/0"): ("SW1", "Eth0/3"),
-    ("R3", "Eth0/1"): ("R4", "Eth0/0"),
-    ("R3", "Eth0/2"): ("R5", "Eth0/0"),
-    ("SW1", "Eth0/1"): ("R1", "Eth0/0"),
-    ("SW1", "Eth0/2"): ("R2", "Eth0/0"),
-    ("SW1", "Eth0/3"): ("R3", "Eth0/0"),
-}
+topology_example = {('R1', 'Eth0/0'): ('SW1', 'Eth0/1'),
+                    ('R2', 'Eth0/0'): ('SW1', 'Eth0/2'),
+                    ('R2', 'Eth0/1'): ('SW2', 'Eth0/11'),
+                    ('R3', 'Eth0/0'): ('SW1', 'Eth0/3'),
+                    ('R3', 'Eth0/1'): ('R4', 'Eth0/0'),
+                    ('R3', 'Eth0/2'): ('R5', 'Eth0/0'),
+                    ('SW1', 'Eth0/1'): ('R1', 'Eth0/0'),
+                    ('SW1', 'Eth0/2'): ('R2', 'Eth0/0'),
+                    ('SW1', 'Eth0/3'): ('R3', 'Eth0/0')}
+
+
+if __name__ == "__main__":
+    T = Topology(topology_example)
+    print(T.topology)
