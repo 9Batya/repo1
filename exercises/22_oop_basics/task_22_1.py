a97b2659fed9fa1663b00dc621150b03744ae76b
9Batya <daniel_gariev@mail.ru>
2024-02-04 12:24:03 +0500

    20.1

--- exercises/22_oop_basics/task_22_1.py
+++ exercises/22_oop_basics/task_22_1.py
@@ -33,14 +33,25 @@ Out[3]:
 
 """
 
-topology_example = {
-    ("R1", "Eth0/0"): ("SW1", "Eth0/1"),
-    ("R2", "Eth0/0"): ("SW1", "Eth0/2"),
-    ("R2", "Eth0/1"): ("SW2", "Eth0/11"),
-    ("R3", "Eth0/0"): ("SW1", "Eth0/3"),
-    ("R3", "Eth0/1"): ("R4", "Eth0/0"),
-    ("R3", "Eth0/2"): ("R5", "Eth0/0"),
-    ("SW1", "Eth0/1"): ("R1", "Eth0/0"),
-    ("SW1", "Eth0/2"): ("R2", "Eth0/0"),
-    ("SW1", "Eth0/3"): ("R3", "Eth0/0"),
-}
+class Topology:
+    def __init__(self,topology_example):
+        keys = list(topology_example.keys())
+        values = list(topology_example.values())
+        for k in keys:
+            if k in values:
+                keys.remove(topology_example[k])
+                del topology_example[k]
+        self.topology = topology_example
+
+topology_example = {('R1', 'Eth0/0'): ('SW1', 'Eth0/1'),
+                    ('R2', 'Eth0/0'): ('SW1', 'Eth0/2'),
+                    ('R2', 'Eth0/1'): ('SW2', 'Eth0/11'),
+                    ('R3', 'Eth0/0'): ('SW1', 'Eth0/3'),
+                    ('R3', 'Eth0/1'): ('R4', 'Eth0/0'),
+                    ('R3', 'Eth0/2'): ('R5', 'Eth0/0'),
+                    ('SW1', 'Eth0/1'): ('R1', 'Eth0/0'),
+                    ('SW1', 'Eth0/2'): ('R2', 'Eth0/0'),
+                    ('SW1', 'Eth0/3'): ('R3', 'Eth0/0')}
+
+T = Topology(topology_example)
+print(T.topology)
